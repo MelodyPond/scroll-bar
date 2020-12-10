07fdc1b4a6fbf60b26f37d6142795d23cdee7365
vera <lrisyf@foxmail.com>
2020-12-10 14:10:32 +0800

    update

--- src/lib/scroll.js
+++ src/lib/scroll.js
@@ -1,6 +1,7 @@
 import scrollBar from './ScrollBar.vue'
-let scroll = {}
-scroll.install = function(Vue,options) {
-  Vue.component(scrollBar,scrollBar)
-}
-export default scroll
+
+export default{
+    components: {
+    scrollBar
+   }
+} 
