c77f4c432e147c4aa5f065a039c26641299716b3
vera <lrisyf@foxmail.com>
2020-12-10 11:48:49 +0800

    init

---
+++ src/App.vue
@@ -0,0 +1,65 @@
+<template>
+  <div id="app">
+    <scroll-bar class="wrapper" :options="options">
+      <div class="inner">
+       <div style="height:800px"></div>
+      </div>
+     </scroll-bar>
+  </div>
+</template>
+
+<script>
+import ScrollBar from './components/ScrollBar.vue'
+
+export default {
+  name: 'App',
+  components: {
+    ScrollBar
+  },
+  data(){
+    return {
+      cc: ['ss'],
+      options: {
+        innerStyle:{
+          backgroundColor: 'red',
+          borderRadius: '5px',
+          width: '5px'
+        },
+        outStyle: {
+        width: '5px',
+        borderRadius: '5px',
+        backgroundColor: 'yellow'
+      },
+      level :80
+      },
+      
+    }
+  },
+  methods: {
+    add(){
+      this.cc.push('hh')
+    }
+  }
+}
+</script>
+
+<style>
+#app {
+  font-family: Avenir, Helvetica, Arial, sans-serif;
+  -webkit-font-smoothing: antialiased;
+  -moz-osx-font-smoothing: grayscale;
+  text-align: center;
+  color: #2c3e50;
+  margin-top: 60px;
+}
+.wrapper{
+  width:200px;height:200px;border:1px solid rgb(94, 126, 94);margin: 0 auto;
+ 
+}
+.inner{
+  background-color: blueviolet;
+  width:100px;
+  border: 3px solid pink;
+  /* height:300px; */
+}
+</style>
---
+++ src/components/ScrollBar.vue
@@ -0,0 +1,185 @@
+<template>
+	<div
+		class="scroll-wrapper"
+		ref="scrollBarWrapper"
+		@wheel.prevent="scrollBarWheel"
+		:style="wrapperStyle"
+	>
+		<div ref="content"><slot></slot></div>
+		<div class="scroll-track" :style="outStyle" v-show="ifShowBar">
+			<div
+				:style="innerStyle"
+				ref="scrollInner"
+				@mousedown="dragBar"
+			></div>
+		</div>
+	</div>
+</template>
+
+<script>
+export default {
+	name: 'ScrollBar',
+	props: {
+		options: Object,
+	},
+	data() {
+		return {
+			ifShowBar: true,
+			innerStyle: {
+				width: '100%',
+				borderRadius: '8px',
+				backgroundColor: 'grey',
+				position: 'absolute',
+				top: '0px',
+			},
+			outStyle: {
+				width: '8px',
+				borderRadius: '10px',
+				backgroundColor: '#fff',
+			},
+			observer: null,
+			top: 0,
+      top2: 0,
+      level: 20,
+			isDown: false,
+			scrollHeight: 0,
+			contentHeight: 0,
+			wrapperHeight: 0,
+			wrapperStyle: {},
+		}
+	},
+	created() {
+		if (this.options.innerStyle) {
+      this.innerStyle.width = this.options.innerStyle.width||'100%'
+      this.innerStyle.backgroundColor = this.options.innerStyle.backgroundColor||'grey'
+      this.innerStyle.borderRadius = this.options.innerStyle.borderRadius||'0px'
+		}
+		if (this.options.outStyle) {
+   //   this.outStyle = this.options.outStyle
+      this.outStyle.width = this.options.outStyle.width||'5px'
+      this.outStyle.backgroundColor = this.options.outStyle.backgroundColor||'#fff'
+      this.outStyle.borderRadius = this.options.outStyle.borderRadius||'0px'
+    }
+    this.level = this.options.level||20
+	},
+	mounted: function() {
+		let that = this
+		this.$refs.content.style.top = '0'
+		that.toShowBar()
+		that.$forceUpdate()
+		this.observer = new MutationObserver(
+			function() {
+				that.toShowBar()
+			}.bind(this)
+		)
+
+		// Setup the observer
+		this.observer.observe(this.$refs.content, {
+			attributes: true,
+			childList: true,
+			characterData: true,
+			subtree: true,
+		})
+	},
+	beforeDestroy: function() {
+		// Clean up
+		this.observer.disconnect()
+	},
+	methods: {
+		scrollBarWheel(e) {
+			let contentHeight = this.$refs.content.offsetHeight
+			let scrollWrapperHeight = this.$refs.scrollBarWrapper.offsetHeight
+			let disd =
+				this.$refs.content.offsetHeight -
+				this.$refs.scrollBarWrapper.offsetHeight
+			let disd2 =(scrollWrapperHeight -(scrollWrapperHeight * scrollWrapperHeight) /contentHeight) /disd
+			if (e.wheelDelta > 0) {
+				if (this.top < -this.level) {
+					this.top += this.level
+					this.top2 -= disd2 * this.level
+				} else {
+					this.top = 0
+					this.top2 = 0
+				}
+				this.$refs.content.style.top = this.top + 'px'
+				this.innerStyle.top = this.top2 + 'px'
+			}
+			if (e.wheelDelta < 0 && this.top + disd > this.level) {
+				this.top -= this.level
+				this.top2 += disd2 * this.level
+				this.$refs.content.style.top = this.top + 'px'
+				this.innerStyle.top = this.top2 + 'px'
+			}
+			if (
+				e.wheelDelta < 0 &&
+				this.top + disd < this.level &&
+				this.top + disd > 0
+			) {
+				this.top = -disd
+				this.$refs.content.style.top = this.top + 'px'
+				this.top2 =
+					scrollWrapperHeight -
+					(scrollWrapperHeight * scrollWrapperHeight) / contentHeight
+				this.innerStyle.top = this.top2 + 'px'
+      }
+      console.log(this.innerStyle)
+		},
+		toShowBar() {
+			this.contentHeight = this.$refs.content.offsetHeight
+			this.wrapperHeight = this.$refs.scrollBarWrapper.offsetHeight
+			if (this.contentHeight > this.wrapperHeight) {
+				this.ifShowBar = true
+				this.$refs.content.style.position = 'absolute'
+				this.innerStyle.height =
+					(this.wrapperHeight / this.contentHeight) * 100 + '%'
+			} else {
+				this.ifShowBar = false
+			}
+		},
+		dragBar() {
+			let rate =
+				(this.contentHeight - this.wrapperHeight) /
+				(this.wrapperHeight - this.$refs.scrollInner.offsetHeight)
+			let that = this
+			let t = that.$refs.scrollBarWrapper.offsetTop
+			document.onmousemove = function() {
+				that.wrapperStyle = {
+					boxShadow: '2px 2px 10px 0px rgba(54,55,55,0.06)',
+				}
+				let h = that.wrapperHeight - that.$refs.scrollInner.offsetHeight
+				if (event.pageY - t > 0 || event.pageY - t == 0) {
+					if (event.pageY - t < h || event.pageY - t == h) {
+						that.top2 = event.pageY - t
+						that.top = -that.top2 * rate
+					}
+				}
+				that.innerStyle.top = that.top2 + 'px'
+				that.$refs.content.style.top = that.top + 'px'
+			}
+			document.onmouseup = function() {
+				that.wrapperStyle = { boxShadow: 'none' }
+				document.onmousemove = null
+			}
+		},
+	},
+}
+</script>
+<style scoped>
+.scroll-wrapper {
+	position: relative;
+	overflow: hidden;
+	-webkit-user-select: none;
+	-moz-user-select: none;
+	-ms-user-select: none;
+	user-select: none;
+}
+.scroll-track {
+	height: 100%;
+	position: absolute;
+	top: 0;
+	right: 0;
+}
+.scroll-inner-bar {
+	position: absolute;
+}
+</style>
